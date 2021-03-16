15b2c45bf776b65f1b5feffb45c1f92ca58ec1f1
Dale Appleby <dale@allhouse.co.nz>
2021-03-16 15:25:40 +1300

    c

--- index.js
+++ index.js
@@ -505,15 +505,17 @@ function login(loginData, options, callback) {
   }
 
   var globalOptions = {
-    forceLogin: true,
-    logLevel: /^win/.test(process.platform) ? "verbose" : "warn",
-    listenEvents: true,
-    selfListen: true,
-    updatePresence: true,
-    autoMarkDelivery: true,
-    autoMarkRead: true,
-    userAgent:
-      "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Mobile Safari/537.36"
+    options: {
+      forceLogin: true,
+      logLevel: /^win/.test(process.platform) ? "verbose" : "warn",
+      listenEvents: true, //
+      selfListen: true,
+      updatePresence: true,
+      autoMarkDelivery: true,
+      autoMarkRead: true,
+      userAgent:
+        "Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Mobile Safari/537.36"
+    }
   };
 
   setOptions(globalOptions, options);
